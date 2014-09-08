f72a51f434fac11e02654e14e8118c20b6a072e7
Eliot Hedeman <ehedeman@barracuda.com>
2014-09-08 19:36:20 -0400

    added benchmark test for set on redis server

--- storage/redis_test.go
+++ storage/redis_test.go
@@ -1,16 +1,32 @@
 package storage
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
-func TestRedis(t *testing.T) {
+// TestSetGet must have a redis server running on localhost:6379
+func TestSetGet(t *testing.T) {
+	expected := "test"
 	r, _ := NewRedis(":6379", 10)
-	r.Set("test", []byte("yup"))
+	r.Set("test", []byte(expected))
 	b, err := r.Get("test")
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(string(b))
+
+	// test that b comes back with the correct value
+	if string(b) != expected {
+		t.Fatal("Expected: " + expected + " got: " + string(b))
+	}
+}
+
+// BenchmarkSet benchmark the SET command to the redis server
+func BenchmarkSetPoolsize10(b *testing.B) {
+	setValue := []byte("set-test-value")
+	r, _ := NewRedis(":6379", 100)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			r.Set("set", setValue)
+		}
+	})
+
 }
